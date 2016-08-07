41e278b753ae0dfe9fd328ea35d25613cef91782
anirudhshenoy <anirudhshenoy92@gmail.com>
2016-08-06 21:43:53 -0400

    Changed the layout of the code
    Made code slightly more readable. Changed parameters for mutation.

--- ga_opt.py
+++ ga_opt.py
@@ -11,14 +11,14 @@ import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
 np.set_printoptions(precision=3,suppress=True)
 
-params = [500, 0.2, 200, 4, 50]
+params = [500, 0.35, 200, 4, 50]
 #plt.axis([0, 260, 0, 2.6])
 #plt.ion()
-#These are just some parameters for the GA, defined below in order:
+#GA Parameters
 # [Init pop (pop=100), mut rate (=5%), num generations (250), chromosome/solution length (3), # winners/per gen]
 
-b_const=[0.1,2]
-c_const=[0.1,0.4]
+b_const=[0.1,5]
+c_const=[0.1,0.35]
 alpha_const=[0,3]
 airfoil_const=[0,639]          #1468 for General AFs
 AR_const=5
@@ -30,23 +30,19 @@ e1=0.85
 e=0.85
 pi=3.1415
 
-#curPop=np.empty([params[0],params[3]])
 
-
-#root=tk.Tk()
-#root.withdraw()
-#file_path=filedialog.askopenfilename()
-#print(file_path)
-
+#Open XLS for Airfoil Data
 wb = openpyxl.load_workbook('C:/Users/Aniru_000/Desktop/TD-1/Airfoil/s1223/airfoil/MasterPolarsFlat/airfoilpolars_master_flat.xlsx')
 print ("Opened File")
 sheet = wb.get_sheet_by_name('slopes')                #Change to index based
 print ("Opened Sheet")
 slopes=[]
 intercepts=[]
-cd_d0=[]
-cd_d1=[]                                        #Please fix this shit
+temp=[]                                       #Please fix this shit
 cd_d2=[]
+cd_d1=[]
+cd_d0=[]
+
 airfoil_names=[]
 
 
@@ -61,8 +57,9 @@ for cellObj in sheet.columns[2]:
 
 sheet = wb.get_sheet_by_name('slopes1')
 
+
 for cellObj in sheet.columns[1]:
-	cd_d2.append(cellObj.value)
+        cd_d2.append(cellObj.value)
 	
 for cellObj in sheet.columns[2]:
 	cd_d1.append(cellObj.value)
@@ -73,39 +70,39 @@ for cellObj in sheet.columns[3]:
 
 #[b c alpha AF]
 def fitness(pop):
+    #Aliases        
     b_pop=pop[0]
     c_pop=pop[1]
     alpha_pop=pop[2]
     AF_pop=pop[3]
-    wing_area=b_pop*c_pop
-    AR=(b_pop**2)/wing_area
-    a_new=slopes[int(AF_pop)]/(pi*e1*AR)
-    a_new=slopes[int(AF_pop)]/(1+(57.3*a_new))
-    cl=intercepts[int(AF_pop)]+(a_new*alpha_pop)
-    cd_i=(cl**2)/(pi*e*AR)
     d2=cd_d2[int(AF_pop)]
     d1=cd_d1[int(AF_pop)]
     d0=cd_d0[int(AF_pop)]
 
-    cd_p=d2*(alpha_pop)**2 + d1*(alpha_pop) + d0
-    lift=0.5*rho*(v**2)*wing_area*cl                    #Add intercept to lift slope predictor
+    #Wing Dimensions
+    wing_area=b_pop*c_pop
+    AR=(b_pop**2)/wing_area
+
+    #Slope Correction    
+    a_new=slopes[int(AF_pop)]/(pi*e1*AR)
+    a_new=slopes[int(AF_pop)]/(1+(57.3*a_new))
+
+    #Lift Calculation
+    cl=intercepts[int(AF_pop)]+(a_new*alpha_pop)
+    lift=0.5*rho*(v**2)*wing_area*cl
+
+    #Drag Calculation
+    cd_i=(cl**2)/(pi*e*AR)
+    cd_p=d2*(alpha_pop)**2 + d1*(alpha_pop) + d0                                                      
     cd=cd_p+cd_i
     drag=0.5*rho*(v**2)*wing_area*cd    
-    #if AR>=AR_const :
-            #Lift
-            #AR
-            #wing_area
-            #
-            #fit=(4/((pop[1]**2)*pop[0])) + (1/drag)                 #Added Alpha +lift/3 
-                                                            #Play around with Lift_fit parameters for convergence
-    lift_fit=100*math.exp(-((lift-lift_const)**2)/(2*10**2))              #Gaussian function centered around lift_constant, A controls height
-    fit=(lift/drag)+lift_fit              #stall angle characteristics  Minimize moment
-    #if lift>=lift_const:
-    #        fit=lift/drag
-        
-    #else:
-    #        fit=-np.inf                         #Replacing this with a fitness function causes problems because b,c for some reason have huge values after Gen1 ? Because of Mutation ?
-                                                #Replacing with a function instead would alow the program to know how bad the individual was, might allow for faster convergence 
+
+
+    #Fitness Value Calculations                                                         #Play around with Lift_fit parameters for convergence
+    lift_fit=70*math.exp(-((lift-lift_const)**2)/(2*7**2))                            #Gaussian function centered around lift_constant, A controls height
+    fit=(lift/drag)+lift_fit                                                            #stall angle characteristics  Minimize moment
+
+
     return fit
                             
 def final_fitness(pop):
@@ -132,20 +129,23 @@ def final_fitness(pop):
     
     
 
+
+
+
+
+#First Generation Initialization
 b =np.random.choice(np.arange(b_const[0],b_const[1],step=0.01),size=(params[0],1),replace=True)
 c =np.random.choice(np.arange(c_const[0],c_const[1],step=0.01),size=(params[0],1),replace=True)
 alpha =np.random.choice(np.arange(alpha_const[0],alpha_const[1],step=0.5),size=(params[0],1),replace=True)
 airfoil =np.random.choice(np.arange(airfoil_const[0],airfoil_const[1],step=1),size=(params[0],1),replace=True)
-
-
-#Implement Constraints as part of fitness function
-
-
-
 curPop=np.concatenate((b,c,alpha,airfoil),1)
 nextPop = np.zeros((curPop.shape[0], curPop.shape[1]))
 fitVec = np.zeros((params[0], 2))
 best_fit_overall=-np.inf
+
+
+#Looping over Generations
+
 
 for j in range (params[2]):
         for i in range(params[0]):
@@ -163,6 +163,8 @@ for j in range (params[2]):
                
         print("(Gen: #%s) Best Fitness: %s" % (j,best_fit))
         print("Airfoil: "+ airfoil_names[int(curPop[best_cand,3])])
+
+        #Hall of Fame
         for n in range(len(winners)):
                              selected = np.random.choice(range(len(fitVec)), params[4]/2, replace=False)
                              wnr = np.argmax(fitVec[selected,1])
@@ -170,8 +172,16 @@ for j in range (params[2]):
 
 
         nextPop[:len(winners)] = winners
+
+        #Crossover
         nextPop[len(winners):] = np.array([np.array(np.random.permutation(np.repeat(winners[:, x], ((params[0] - len(winners))/len(winners)), axis=0))) for x in range(winners.shape[1])]).T
-        curPop = np.multiply(nextPop, np.matrix([np.float(np.random.uniform(0,2)) if random.random() < params[1] else 1 for x in range(nextPop.size)]).reshape(nextPop.shape))
+
+        #Mutation
+        curPop = np.multiply(nextPop, np.matrix([np.float(np.random.uniform(0.5,1.)) if random.random() < params[1] else 1 for x in range(nextPop.size)]).reshape(nextPop.shape))
+
+
+
+        #Ensure all values are within constraints
         for k in range(params[0]):
                             curPop[k,3]=math.floor(math.fabs(curPop[k,3]))
                             if curPop[k,3]>airfoil_const[1]:
@@ -183,15 +193,9 @@ for j in range (params[2]):
                             if curPop[k,0]>b_const[1] or curPop[k,0]<b_const[0]:
                                     curPop[k,0]=np.random.uniform(b_const[0],b_const[1])
 
-        #plt.scatter(j, best_b)
-        #plt.pause(0.005)
-
-                            
+                                       
 
 
 print("Best Sol'n:\n%s" % (best_soln))
 print("Airfoil: "+airfoil_names[int(best_soln[0,3])])
 final_fitness(np.array([best_soln[0,0],best_soln[0,1],best_soln[0,2],best_soln[0,3]]))
-
-#while True:
-#    plt.pause(0.001)
