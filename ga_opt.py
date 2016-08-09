b8cd1169beae65a44facb8a1b02a3f9003799051
anirudhshenoy <anirudhshenoy92@gmail.com>
2016-08-08 22:36:01 -0400

    Added Wing Taper Calculations

--- ga_opt.py
+++ ga_opt.py
@@ -11,22 +11,23 @@ import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
 np.set_printoptions(precision=3,suppress=True)
 
-params = [500, 0.35, 200, 4, 50]
+params = [500, 0.05, 200, 5, 50]
 #plt.axis([0, 260, 0, 2.6])
 #plt.ion()
 #GA Parameters
 # [Init pop (pop=100), mut rate (=5%), num generations (250), chromosome/solution length (3), # winners/per gen]
 
-b_const=[0.1,2.5]
-c_const=[0.1,0.4]
+b_const=[0.1,2]
+c_const=[0.1,0.3]
+taper_const=[0.5,0.9]
 alpha_const=[0,3]
 airfoil_const=[0,635]          #1468 for General AFs
 AR_const=5
-lift_const=40
+lift_const=37
 area_const=1
 rho=1.227
 v=10
-e1=0.85
+e1=0.9
 e=0.85
 pi=3.1415
 
@@ -84,13 +85,14 @@ for cellObj in sheet.columns[3]:
 
 
 
-#[b c alpha AF]
+#[b c taper alpha AF]
 def fitness(pop):
     #Aliases        
     b_pop=pop[0]
     c_pop=pop[1]
-    alpha_pop=pop[2]
-    AF_pop=pop[3]
+    taper=pop[2]
+    alpha_pop=pop[3]
+    AF_pop=pop[4]
     d2=cd_d2[int(AF_pop)]
     d1=cd_d1[int(AF_pop)]
     d0=cd_d0[int(AF_pop)]
@@ -99,7 +101,8 @@ def fitness(pop):
     m0=cm_m0[int(AF_pop)]
 
     #Wing Dimensions
-    wing_area=b_pop*c_pop
+    MAC=(2/3)*c_pop*((taper**2 + taper +1)/(taper+1))
+    wing_area=b_pop*MAC
     AR=(b_pop**2)/wing_area
 
     #Slope Correction    
@@ -120,7 +123,7 @@ def fitness(pop):
     #cm=m2*(alpha_pop)**2 + m1*(alpha_pop) + m0
 
     #Fitness Value Calculations                                                         #Play around with Lift_fit parameters for convergence
-    lift_fit=70*math.exp(-((lift-lift_const)**2)/(2*20**2))   #70,7                         #Gaussian function centered around lift_constant, A controls height
+    lift_fit=70*math.exp(-((lift-lift_const)**2)/(2*7**2))   #70,7                         #Gaussian function centered around lift_constant, A controls height
     fit=(lift/drag)+lift_fit #+ (1/b_pop)*5                                                        #stall angle characteristics  Minimize moment
 
 
@@ -129,9 +132,12 @@ def fitness(pop):
 def final_fitness(pop):
     b_pop=pop[0]
     c_pop=pop[1]
-    alpha_pop=pop[2]
-    AF_pop=int(pop[3])
-    wing_area=b_pop*c_pop
+    taper=pop[2]
+    alpha_pop=pop[3]
+    AF_pop=int(pop[4])
+
+    MAC=(2/3)*c_pop*((taper**2 + taper +1)/(taper+1))
+    wing_area=b_pop*MAC
     AR=(b_pop**2)/wing_area
     a_new=slopes[int(AF_pop)]/(pi*e1*AR)
     a_new=slopes[int(AF_pop)]/(1+(57.3*a_new))
@@ -157,9 +163,10 @@ def final_fitness(pop):
 #First Generation Initialization
 b =np.random.choice(np.arange(b_const[0],b_const[1],step=0.01),size=(params[0],1),replace=True)
 c =np.random.choice(np.arange(c_const[0],c_const[1],step=0.01),size=(params[0],1),replace=True)
+taper =np.random.choice(np.arange(taper_const[0],taper_const[1],step=0.01),size=(params[0],1),replace=True)
 alpha =np.random.choice(np.arange(alpha_const[0],alpha_const[1],step=0.5),size=(params[0],1),replace=True)
 airfoil =np.random.choice(np.arange(airfoil_const[0],airfoil_const[1],step=1),size=(params[0],1),replace=True)
-curPop=np.concatenate((b,c,alpha,airfoil),1)
+curPop=np.concatenate((b,c,taper,alpha,airfoil),1)
 nextPop = np.zeros((curPop.shape[0], curPop.shape[1]))
 fitVec = np.zeros((params[0], 2))
 best_fit_overall=-np.inf
@@ -171,7 +178,7 @@ best_fit_overall=-np.inf
 for j in range (params[2]):
         for i in range(params[0]):
                              fitVec[i,0]=i
-                             fitVec[i,1]=fitness(np.array([curPop[i,0],curPop[i,1],curPop[i,2],curPop[i,3]]))
+                             fitVec[i,1]=fitness(np.array([curPop[i,0],curPop[i,1],curPop[i,2],curPop[i,3],curPop[i,4]]))           #Needs Change
 
         winners = np.zeros((params[4], params[3]))
         best_fit=max(fitVec[:,1])
@@ -204,19 +211,21 @@ for j in range (params[2]):
 
         #Ensure all values are within constraints
         for k in range(params[0]):
-                            curPop[k,3]=math.floor(math.fabs(curPop[k,3]))
-                            if curPop[k,3]>airfoil_const[1]:
-                                    curPop[k,3]=random.randint(0,airfoil_const[1])
-                            if curPop[k,2]>alpha_const[1] or curPop[k,2]<alpha_const[0]:
-                                    curPop[k,2]=np.random.uniform(alpha_const[0],alpha_const[1])
+                            curPop[k,4]=math.floor(math.fabs(curPop[k,4]))
+                            if curPop[k,4]>airfoil_const[1]:
+                                    curPop[k,4]=random.randint(0,airfoil_const[1])
+                            if curPop[k,3]>alpha_const[1] or curPop[k,2]<alpha_const[0]:
+                                    curPop[k,3]=np.random.uniform(alpha_const[0],alpha_const[1])
                             if curPop[k,1]>c_const[1] or curPop[k,1]<c_const[0]:
                                     curPop[k,1]=np.random.uniform(c_const[0],c_const[1])
                             if curPop[k,0]>b_const[1] or curPop[k,0]<b_const[0]:
                                     curPop[k,0]=np.random.uniform(b_const[0],b_const[1])
+                            if curPop[k,2]>taper_const[1] or curPop[k,2]<taper_const[0]:
+                                    curPop[k,2]=np.random.uniform(taper_const[0],taper_const[1])
 
                                        
 
 
 print("Best Sol'n:\n%s" % (best_soln))
-print("Airfoil: "+airfoil_names[int(best_soln[0,3])])
-final_fitness(np.array([best_soln[0,0],best_soln[0,1],best_soln[0,2],best_soln[0,3]]))
+print("Airfoil: "+airfoil_names[int(best_soln[0,4])])
+final_fitness(np.array([best_soln[0,0],best_soln[0,1],best_soln[0,2],best_soln[0,3],best_soln[0,4]]))
